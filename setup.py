d859edca8f1800968cdc655fc83494fdab8820a5
siebe <siebe@pluxbox.nl>
2017-08-15 17:34:17 +0200

    Release of version 1.0.96

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ import sys
 from setuptools import setup, find_packages
 
 NAME = "Pluxbox RadioManager Client"
-VERSION = "1.0.95"
+VERSION = "1.0.96"
 # To install the library, run the following
 #
 # python setup.py install
